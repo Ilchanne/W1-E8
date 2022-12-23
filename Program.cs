d0bbb291d62657e5c91d028ddef7ba8a410d4f17
Ilchan <annilchen@yandex.ru>
2022-12-23 22:59:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Double Forengeit;
+Console.Title = ("Калькулятор");
+Console.WriteLine("Введите цельсии");
+Forengeit = Convert.ToDouble(Console.ReadLine());
+Console.Write("По форенгейту = ");
+Console.Write(Forengeit * 9 / 5 + 32);
+Console.ReadKey();
